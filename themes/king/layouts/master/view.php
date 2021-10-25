5860d6a497437fcebac939cf0f13bc554e85856c
navid <navidsky10@gmail.com>
2021-10-25 05:13:25 +0330

    fix

---
+++ themes/king/layouts/master/view.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://technext.github.io/marshmallow/vendors/mdi/css/materialdesignicons.min.css">
+		<link rel="stylesheet" href="https://technext.github.io/marshmallow/vendors/owl.carousel/css/owl.carousel.css">
+		<link rel="stylesheet" href="https://technext.github.io/marshmallow/vendors/owl.carousel/css/owl.theme.default.min.css">
+		<link rel="stylesheet" href="https://technext.github.io/marshmallow/vendors/aos/css/aos.css">
+		<link rel="stylesheet" href="https://technext.github.io/marshmallow/vendors/jquery-flipster/css/jquery.flipster.css">
+		<link rel="stylesheet" href="https://technext.github.io/marshmallow/css/style.css">
+		<link rel="shortcut icon" href="images/favicon.png" />
+
+    <title><?= $page->get('title') ?></title>
+    <link rel="stylesheet" href="<?= phpb_theme_asset('css/style.css') ?>" />
+</head>
+<body >
+		<div id="mobile-menu-overlay"></div>
+
+        <div class="col-md-4">header</div>
+<?= $body ?>
+<div class="col-md-4">footer</div>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://technext.github.io/marshmallow/vendors/base/vendor.bundle.base.js"></script>
+		<script src="https://technext.github.io/marshmallow/vendors/owl.carousel/js/owl.carousel.js"></script>
+		<script src="https://technext.github.io/marshmallow/vendors/aos/js/aos.js"></script>
+		<script src="https://technext.github.io/marshmallow/vendors/jquery-flipster/js/jquery.flipster.min.js"></script>
+		<script src="https://technext.github.io/marshmallow/js/template.js"></script>
+</body>
+</html>
